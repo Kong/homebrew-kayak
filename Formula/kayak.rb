fafaab0b6bb17152fe254d762c062edfc0efc850
Emiliano <emiliano.lesende@gmail.com>
2023-01-05 18:53:53 -0300

    ci: kayak 0.30.0

--- Formula/kayak.rb
+++ Formula/kayak.rb
@@ -3,10 +3,10 @@ require_relative "lib/private_strategy"
 class Kayak < Formula
     desc "Kayak transformation language targeting proxy-wasm"
     homepage "https://github.com/Kong/kayak"
-    version "0.29.0"
+    version "0.30.0"
   
-    url "https://github.com/Kong/kayak/releases/download/v0.29.0/kayak-macos-x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "1c7c2e2fb00237df966cded0aa1770e036ebe71b12b49cf18851392f47e4d67e"
+    url "https://github.com/Kong/kayak/releases/download/v0.30.0/kayak-macos-x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "37b984dfceba6a655d13dc9c8eb86d6640c9f0749322c2a22fcc4c3a20217181"
   
     def install
         bin.install "kayak"
